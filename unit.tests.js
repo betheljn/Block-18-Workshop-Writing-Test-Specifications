86cc3db482e0e95e9925b933bbd9b3f53e07be38
betheljn <151068353+betheljn@users.noreply.github.com>
2023-12-18 10:09:40 -0500

    Add files via upload

---
+++ unit.tests.js
@@ -0,0 +1,9 @@
+Test (`Multiplying different integers that gives a correct result`, () => {
+    expect(multiplication (2,3)) .toBe (6);
+    expect (multiplication (0,10)) .toBe (0);
+})
+
+// Functional Test
+test('Ensure concatenation of odd numbers works correctly in the application', async () => {}
+);
+
